e94b0043737d6347967b0285c835e434deb2daea
Matheus <mateusanches2@gmail.com>
2020-09-23 20:38:17 -0300

    Protected Route Fix

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Navigate, Route } from 'react-router';
+import { Navigate, Route } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 
 const ProtectedRoute = (props) => {
-  const { login } = React.userContext(UserContext);
+  const { login } = React.useContext(UserContext);
 
   if (login === true) return <Route {...props} />;
   else if (login === false) return <Navigate to="/login" />;
